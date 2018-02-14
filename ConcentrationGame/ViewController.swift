c362a5f6d8b10b78a5403d9d8117bd0ef78ff9e7
akashsoni <akashsoni8285@gmail.com>
2018-02-14 15:52:11 +0530

    make a print statement

--- ConcentrationGame/ViewController.swift
+++ ConcentrationGame/ViewController.swift
@@ -13,12 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("hello world")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
