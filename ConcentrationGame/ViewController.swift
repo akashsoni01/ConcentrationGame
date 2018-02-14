686a5b135a37a1a9975f3898e86304167e0d474c
akashsoni <akashsoni8285@gmail.com>
2018-02-14 17:00:00 +0530

    flip count display

--- ConcentrationGame/ViewController.swift
+++ ConcentrationGame/ViewController.swift
@@ -11,11 +11,22 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var cardButtons: [UIButton]!
     var flipCount:Int = 0
-    
+    @IBOutlet weak var flipCountDisplay: UILabel!
+    var emojies = ["🎃","👻","🎃","👻"]
     @IBAction func cardTouchAction(_ sender: UIButton) {
-    }
-    func flipCard(){
+        if let cardNumber = cardButtons.index(of:sender){
+            flipCard(at: cardNumber, on: sender)
+        }
         flipCount += 1
+    }
+    func flipCard(at index:Int,on button:UIButton){
+        if button.currentTitle == emojies[index]{
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+        }else{
+            button.setTitle(emojies[index], for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
     }
 }
 
